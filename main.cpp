9d370f8738d632dcbd2da9a2aa50fb249d95769e
TylerOrcutt <tyorcutt@gmail.com>
2019-06-01 16:07:04 -0400

    added some cmd args

--- main.cpp
+++ main.cpp
@@ -47,10 +47,41 @@ void handleArgs(int args,char ** argv){
 			TransmissionRC::addTorrent(std::string(argv[i+1]));	
 		//	std::cout<<argv[i+1]<<std::endl;
 		}
+	
+		if(std::string(argv[i]) == "-t"){
+			std::vector<rcTorrent> *torrents = getTorrents();
+			for(int t=0;torrents!=NULL && t<torrents->size();t++){
+			
+				std::cout<<(*torrents)[t].ID<<":"
+					 <<(*torrents)[t].Name
+					 <<" "<<(*torrents)[t].totalSize/1024/1024<<"MB"
+					 <<" "<<c_trStatus[(*torrents)[t].Status]
+					 <<" "<<(*torrents)[t].percentDone
+					 <<std::endl;
+			}
+			i--;
+		}
+	
+		
+		if(std::string(argv[i]) == "-r"){
+			int id=0;	
+			if((id=std::stoi(argv[i+1]))>0){
+				std::cout<<"resuming "<<id<<"\r\n";
+				resumeTorrent(id);
 
+			}
+		}
 
+		if(std::string(argv[i]) == "-s"){
+			int id=0;	
+			if((id=std::stoi(argv[i+1]))>0){
+				std::cout<<"stopping "<<id<<"\r\n";
+				stopTorrent(id);
+
+			}
+		}
 	}
-	if(args==2){
+	if(args==2 && std::strlen(argv[1])>2){
 		TransmissionRC::addTorrent(std::string(argv[1]));	
 	}
 }
